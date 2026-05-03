08b7587d220827aeec7bd5e2b306b2ee66f74fb7
Tadakala Likitha <76098994+likithatadakala@users.noreply.github.com>
2026-05-03 17:06:59 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        
+        for price in prices:
+            # Update the lowest price seen so far
+            if price < min_price:
+                min_price = price
+            # Otherwise, check if selling today gives a better profit
+            else:
+                profit = price - min_price
+                max_profit = max(max_profit, profit)
+        
+        return max_profit
+        
